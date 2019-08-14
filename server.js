8798f5826abe4f1bc1a7cb7e3ec7e1db70b0c948
robertvenanzi <robertgvenanzi@gmail.com>
2019-08-13 20:26:44 -0700

    created first api req for platform and gamertag

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
-const app = express();
+
 
 //Load Config Files
 dotenv.config({ path: './config.env' });
+
+const app = express();
+
+app.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
+    console.log(req.params.platform, req.params.gamertag)
+    res.send('hello');
+});
 
 const port = process.env.PORT || 8000;
 
