fa580bcd3f220e937b68ccb50502b68c437416f5
robertvenanzi <robertgvenanzi@gmail.com>
2019-08-14 18:49:57 -0700

    created api route for profile and errors

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ dotenv.config({ path: './config.env' });
 
 const app = express();
 
-app.get('/api/v1/profile/:platform/:gamertag', (req, res) => {
-    console.log(req.params.platform, req.params.gamertag)
-    res.send('hello');
-});
+// Dev Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+//Profile Routes
+app.use('/api/v1/profile', require('./routes/profile'));
 
 const port = process.env.PORT || 8000;
 
