cc818ea2b42e059fd71dda6527c20c40b4108f7b
robertvenanzi <robertgvenanzi@gmail.com>
2019-08-15 19:04:16 -0700

    Created Header component

---
+++ client/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header>
+    <img :src="image" />
+  </Header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      image: require("@/assests/logo.png")
+    };
+  }
+};
+</script>
+
+<style></style>
